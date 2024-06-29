0dfdf90860a65b3dc13d798f75317aebf16b5c21
int4_t <rewardedivan@gmail.com>
2024-06-29 15:32:51 +0300

    add footer
    trying to not get sued, thanks chatgpt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -51,16 +51,16 @@
 	</div>
 {/if}
 
-<div class="min-h-screen flex flex-col">
-	<div class="navbar bg-base-200">
-		<div class="flex flex-row items-center gap-2">
-			<span class="btn btn-ghost max-sm:btn-sm text-xl" use:copy={"int4_t"} on:svelte-copy={() => copied = true}><DiscordIcon /> discord</span>
-			<a class="btn btn-ghost max-sm:btn-sm text-xl" href="https://x.com/uint4_t"><XIcon /> twitter</a>
-			<a class="btn btn-ghost max-sm:btn-sm text-xl" href="https://github.com/RewardedIvan"><GithubIcon /> github</a>
-			<span class="btn btn-ghost max-sm:btn-sm text-xl" use:copy={"A16B98815D0563295CB2795FA16F1618AADEFEDF"} on:svelte-copy={() => copied = true}><KeyIcon /> pgp</span>
-		</div>
+<div class="navbar bg-base-200">
+	<div class="flex flex-row items-center gap-2">
+		<span class="btn btn-ghost max-sm:btn-sm text-xl" use:copy={"int4_t"} on:svelte-copy={() => copied = true}><DiscordIcon /> discord</span>
+		<a class="btn btn-ghost max-sm:btn-sm text-xl" href="https://x.com/uint4_t"><XIcon /> twitter</a>
+		<a class="btn btn-ghost max-sm:btn-sm text-xl" href="https://github.com/RewardedIvan"><GithubIcon /> github</a>
+		<span class="btn btn-ghost max-sm:btn-sm text-xl" use:copy={"A16B98815D0563295CB2795FA16F1618AADEFEDF"} on:svelte-copy={() => copied = true}><KeyIcon /> pgp</span>
 	</div>
+</div>
 
+<div class="min-h-screen flex flex-col my-5">
 	<div class="flex-grow flex flex-col items-center justify-center gap-5 mx-5">
 		<div class="hero bg-base-200 w-fit p-3 rounded-lg">
 			<div class="hero-content flex-col">
@@ -111,3 +111,14 @@
 		</div>
 	</div>
 </div>
+
+<footer class="footer bg-base-200 text-base-content p-10">
+	<nav>
+		<h6 class="footer-title">LEGAL DISCLAIMER</h6>
+		<span>
+			All product names, logos, brands, trademarks, and registered trademarks are the property of their respective owners. All company, product, and service names used on this website are for identification purposes only. My use of these names, trademarks, and brands does not imply endorsement.
+			I am not affiliated with, endorsed by, or sponsored by any of the companies whose logos and names appear on this site. The icons and names are used solely for reference purposes to provide information about the respective companies.
+			If any trademark owner has any concerns or objections to the use of their trademark, please contact me at <kbd class="kbd kbd-xs">rewardedivan&#64;gmail&#46;com</kbd>, and I will address the issue promptly.
+		</span>
+	</nav>
+</footer>
